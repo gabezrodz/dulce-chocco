5b72b90d8b7f6571c024949e02bf208dfe87fb2a
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-03-11 19:14:35 -0300

    Feat: adicionado resposta de erro para o usuario

--- src/components/Layout/Chocolates/AvaliableChocolates/AvaliableChocolates.js
+++ src/components/Layout/Chocolates/AvaliableChocolates/AvaliableChocolates.js
@@ -1,17 +1,23 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { Chocolates } from "./Style";
 import ChocolateItem from "../ChocolateItem/ChocolateItem";
 import Card from "../../../Shared/Card/Card";
 
 const AvaliableChocolates = () => {
-  const [chocolates, setChocolates] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [chocolates, setChocolates] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
   useEffect(() => {
     const fetchMeals = async () => {
-      
       const response = await fetch(
         "https://dulce-chocco-default-rtdb.firebaseio.com/chocolates.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Ops! Alguma coisa deu errado!");
+      }
+
       const responseData = await response.json();
 
       const loadedChocolates = [];
@@ -25,20 +31,36 @@ const AvaliableChocolates = () => {
           image: responseData[key].image,
         });
       }
-      setIsLoading(false)
-      setChocolates(loadedChocolates)
+      setIsLoading(false);
+      setChocolates(loadedChocolates);
     };
 
-    fetchMeals();
+   
+      fetchMeals().catch(error =>{
+        setIsLoading(false)
+        setHttpError(error.message)
+      })
+    
+     
+    
   }, []);
 
-    if(isLoading){
-      return(
-        <section style={{textAlign: 'center'}}>
-          <h3 style={{color:"white"}}>Carregando...</h3>
-        </section>
-      )
-    }
+  if (isLoading) {
+    return (
+      <section style={{ textAlign: "center" }}>
+        <h3 style={{ color: "white" }}>Carregando...</h3>
+      </section>
+    );
+  }
+
+  if (httpError){
+    return (
+      <section style={{ textAlign: "center" }}>
+        <h3 style={{ color:  "red" }}>Erro de conexão com o banco de dados!</h3>
+      </section>
+    )
+  }
+
 
   const chocolatesList = chocolates.map((chocolate) => (
     <ChocolateItem
